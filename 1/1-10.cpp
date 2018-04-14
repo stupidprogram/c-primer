e7b1a7b8b19a6e78d50aee5d8858de53bc9dbe2f
stupidprogram <rzmsn103@163.com>
2018-04-14 23:12:15 +0800

    Create 1-10.cpp

---
+++ 1/1-10.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int val = 10;
+    while (val >= 0)
+    {
+        cout << val-- << "  ";
+    }
+    cout << endl;
+    return 0;
+}
