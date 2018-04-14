bebe9bb77bf2ff4c995cd04994500f59ab31ce37
stupidprogram <rzmsn103@163.com>
2018-04-14 23:01:37 +0800

    Create 1-4.cpp

---
+++ 1/1-4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << "两个数的和为：" << a + b << endl;
+    cout << "两个数的积为：" << a * b << endl;
+    return 0;
+}
