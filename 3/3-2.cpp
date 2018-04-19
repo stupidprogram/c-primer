1ab9de7d8c7e51f745195b9430c140ea4394b3af
stupidprogram <rzmsn103@163.com>
2018-04-19 22:40:25 +0800

    Create 3-2.cpp

---
+++ 3/3-2.cpp
@@ -0,0 +1,36 @@
+/* 1 */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+   string s1;
+   while(getline(cin,s1))
+   {
+       cout << s1 << endl;
+   }
+    return 0;
+}
+
+
+
+/* 2 */
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+   string s1;
+   while(cin >> s1)
+   {
+       cout << s1 << endl;
+   }
+    return 0;
+}
