777b4297ee1a12053ed30ac9581cd454bb1bb43d
stupidprogram <rzmsn103@163.com>
2018-05-01 22:11:33 +0800

    Create 3-31.cpp

---
+++ 3/3-31.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int a[10];
+
+    for (size_t i = 0; i != 10; ++i)
+    {
+        a[i] = i;
+    }
+    
+    return 0;
+}
