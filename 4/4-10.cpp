da5889bdd0a874dfe9c75f9072496fa761b71b42
stupidprogram <rzmsn103@163.com>
2018-05-04 21:23:14 +0800

    Create 4-10.cpp

---
+++ 4/4-10.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int number;
+    
+    while (cin >> number)
+    {
+        if (number == 42)
+            break;
+    }
+
+    return 0;
+}
