9b74c0665db8791dbc3d31b1aaf663ca99fe30d5
stupidprogram <rzmsn103@163.com>
2018-05-08 23:02:23 +0800

    Update 4-21.cpp

--- 4/4-21.cpp
+++ 4/4-21.cpp
@@ -1,3 +1,5 @@
+//1
+
 #include <iostream>
 #include <vector>
 #include <string>
@@ -15,6 +17,29 @@ int main() {
     for (const auto &i : vec)
     {
         cout << i << "  ";
+    }
+    cout << endl;
+    
+    return 0;
+}
+
+
+
+
+//2  只翻倍输出，不改变vector中的值
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+    vector<int> vec{0,1,2,3,4,5,6,7,8,9};
+
+    for (const auto &i : vec)
+    {
+        cout << (i % 2 ? i * 2 : i) << "  ";
     }
     cout << endl;
     
